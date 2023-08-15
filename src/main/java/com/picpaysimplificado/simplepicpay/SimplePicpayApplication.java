49954562a510d4c0755c9601bfda9cc3e436a7a1
jeferson.santos <j4.oliver23@gmail.com>
2023-08-15 17:06:33 -0300

    Desafio Back-end PicPay

---
+++ src/main/java/com/picpaysimplificado/simplepicpay/SimplePicpayApplication.java
@@ -0,0 +1,13 @@
+package com.picpaysimplificado.simplepicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePicpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePicpayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/picpaysimplificado/simplepicpay/SimplePicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaysimplificado.simplepicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePicpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
